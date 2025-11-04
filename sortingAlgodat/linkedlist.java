6d036991bc33ad8891decbdc1e117d2570477093
tunaabakar <ridhohidayat11111@gmail.com>
2025-11-04 09:14:35 +0800

    Add files via upload

---
+++ sortingAlgodat/linkedlist.java
@@ -0,0 +1,40 @@
+public class linkedlist {
+    private node head;
+
+    public void add(int data) {
+        node newNode = new node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+            public node getHead() {
+            return head;
+        }
+
+        public int size() {
+            int count = 0;
+            node temp = head;
+            while (temp != null) {
+                count++;
+                temp = temp.next;
+            }
+            return count;
+        }
+
+        public void print() {
+            node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+}
+
